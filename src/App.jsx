5d592ba0e5f96850301436a8ed1da380163337f3
nahidthenh <nhgroup18@gmail.com>
2023-06-19 08:08:20 +0600

    Dependency init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+function App() {
+
+  return (
+    <>
+      <Container>
+        <h2 className='bg-danger'>hello</h2>
+      </Container>
+    </>
+  )
+}
+
+export default App
