9c65067ffebaf8e385e1f6a0eeb599b9324db5cd
nahidthenh <nhgroup18@gmail.com>
2023-06-19 09:40:03 +0600

    Home page => hero area content done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,25 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h3>This is home page</h3>
+        <div className="hero-area-wrap">
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <div className="hero-area-content">
+                            <h1>Choose and Play thousands of expert Quiez now.</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                            </p>
+                            <div className="hero-btn-group">
+                                <Link to=''>Play Now</Link>
+                                <Link to=''>Sign Up</Link>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
