cfdd79efb83dd7367ea2f1a9f47c2f1f88f9df21
nahidthenh <nhgroup18@gmail.com>
2023-06-19 11:00:09 +0600

    Quiz Data load and display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,63 @@
 import { Col, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import heroImage from "../../assets/images/hero.jpg"
+import Quiz from "../Quiz/Quiz";
 const Home = () => {
+
+    const quizcatagory = useLoaderData()
+    console.log(quizcatagory);
     return (
-        <div className="hero-area-wrap">
-            <Container>
-                <Row className="align-items-center">
-                    <Col md={6}>
-                        <div className="hero-area-content">
-                            <h1>Choose and Play thousands of expert Quiez now.</h1>
-                            <p>
-                                Smarty Pants is dolor amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </p>
-                            <div className="hero-btn-group">
-                                <Link to=''>Play Now</Link>
-                                <Link to=''>Sign Up</Link>
+        <>
+            <div className="hero-area-wrap">
+                <Container>
+                    <Row className="align-items-center">
+                        <Col md={6}>
+                            <div className="hero-area-content">
+                                <h1>Choose and Play thousands of expert Quiez now.</h1>
+                                <p>
+                                    Smarty Pants is dolor amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                                </p>
+                                <div className="hero-btn-group">
+                                    <Link to=''>Play Now</Link>
+                                    <Link to=''>Sign Up</Link>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                    <Col md={6}>
-                        <div className="hero-area-image">
-                            <img className="img-fluid" src={heroImage} alt="" />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                        </Col>
+                        <Col md={6}>
+                            <div className="hero-area-image">
+                                <img className="img-fluid" src={heroImage} alt="" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+            <div className="quiz-data-wrap">
+                <Container>
+                    <Row>
+                        <Col md={6} className="mx-auto text-center">
+                            <div className="section-title">
+                                <h3>Quiz Categories</h3>
+                                <p>
+                                    Smarty Pants is a sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {
+                            quizcatagory.data.map(quizItem =>
+                                <Col key={quizItem.id} xl={3} lg={4} >
+                                    <Quiz
+                                        item={quizItem}
+                                    />
+                                </Col>
+                            )
+                        }
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 };
 
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+const Quiz = ({ item }) => {
+    console.log(item)
+    return (
+        <div>
+            <h3>Hello</h3>
+        </div>
+    );
+};
+
+export default Quiz;
