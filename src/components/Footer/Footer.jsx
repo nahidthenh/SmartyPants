a4110bfb8f4f1b2f02300d2246c9835f2ad690da
nahidthenh <nhgroup18@gmail.com>
2023-06-22 13:05:09 +0600

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer</h3>
+        <div className="footer-wrap">
+            <h3>© Smarty Pants  2023</h3>
+            {/* <div className="footer-social-link">
+                <ul>
+                    <li><a href="https://www.facebook.com/nahidthenh"><i className="ri-facebook-line"></i></a></li>
+                    <li><a href="https://www.instagram.com/nahidthenh"><i className="ri-instagram-line"></i></a></li>
+                    <li><a href="https://www.youtube.com/@ndevs"><i className="ri-youtube-line"></i></a></li>
+                </ul>
+            </div> */}
         </div>
     );
 };
--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
-// import Footer from "../components/Footer/Footer"
+import Footer from "../components/Footer/Footer"
 const main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
