122c7354e6e285ad72472718d3a99199e6f5384d
nahidthenh <nhgroup18@gmail.com>
2023-06-25 12:26:19 +0600

    Code Update

--- src/components/Blog/BlogDetails.jsx
+++ src/components/Blog/BlogDetails.jsx
@@ -1,14 +1,16 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 
 const BlogDetails = () => {
     let blogs = useLoaderData()
-    // let blog = blogs[`${blogs.id}`];
-    console.log(blogs);
+    // console.log(blogs);
+    const blogId = useParams().id
+    let currentBlog = blogs.filter(blog => blog.id == blogId)
+    let blog = currentBlog[0];
+
     return (
         <div>
-            <h1>length : {blogs.length}</h1>
-            <h3>Title {blogs.title}</h3>
+            <h3>Title {blog.title}</h3>
         </div>
     );
 };
