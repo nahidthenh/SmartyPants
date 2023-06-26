3889c341cfde1523ce2434545d335e5da210c2e3
nahidthenh <nhgroup18@gmail.com>
2023-06-26 07:27:30 +0600

    Google User sign in and Tost message done

--- src/components/Auth/Signup/Signup.jsx
+++ src/components/Auth/Signup/Signup.jsx
@@ -1,7 +1,58 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../../firebase/firebase.init";
+import { useState } from "react";
+
+const googleProvider = new GoogleAuthProvider();
+const auth = getAuth(app);
+// const notify = () => toast("Wow so easy!");
 const Signup = () => {
+    const [user, setUser] = useState({})
+    const [success, setSuccess] = useState(false)
+    console.log(success);
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const user = result.user;
+                setUser(user)
+                setSuccess(
+                    toast.success("You are successfully logged in", {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    })
+                )
+
+            }).catch((error) => {
+                console.error('Error', error)
+                setSuccess(
+                    toast.error("Sorry something went wrong !", {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    })
+                )
+            });
+    }
+
     return (
         <div>
             <h3>This is Sign Up</h3>
+            <h2>Name:{user.displayName}</h2>
+            <img src={user.photoURL} alt="" />
+            <button onClick={() => handleGoogleLogin()}>Login with google</button>
+            <ToastContainer />
         </div>
     );
 };
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDq5kI6iH0frAfshHQISA5T3M75PcE-whk",
+    authDomain: "smarty-pants-c3267.firebaseapp.com",
+    projectId: "smarty-pants-c3267",
+    storageBucket: "smarty-pants-c3267.appspot.com",
+    messagingSenderId: "518988122386",
+    appId: "1:518988122386:web:34a252244ad227318bcbcb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
